72317f25beb709647467f595f08091bd228fca01
Zarkonnen <david.stark@zarkonnen.com>
2016-05-31 15:19:42 +0200

    Escapes support for PHP.

--- seleniumbuilder/content/html/builder/selenium2/io/formats/phpunit.js
+++ seleniumbuilder/content/html/builder/selenium2/io/formats/phpunit.js
@@ -264,9 +264,83 @@ builder.selenium2.io.addLangFormatter({
   },
 
   /**
-   * @see php.js
+   * Processes a parameter value into an appropriately escaped expression. Mentions of variables
+   * with the ${foo} syntax are transformed into expressions that concatenate the variables and
+   * literals.  
+   * For example:
+   * a${b}c
+   * becomes:
+   * "a" . b . "c"
+   * 
    */
   escapeValue: function(stepType, value, pName) {
+    var keysMap = {
+      "NULL": "\\u{E000}",
+      "CANCEL": "\\u{E001}",
+      "HELP": "\\u{E002}",
+      "BACK_SPACE": "\\u{E003}",
+      "TAB": "\\u{E004}",
+      "CLEAR": "\\u{E005}",
+      "RETURN": "\\u{E006}",
+      "ENTER": "\\u{E007}",
+      "SHIFT": "\\u{E008}",
+      "LEFT_SHIFT": "\\u{E008}",
+      "CONTROL": "\\u{E009}",
+      "LEFT_CONTROL": "\\u{E009}",
+      "ALT": "\\u{E00A}",
+      "LEFT_ALT": "\\u{E00A}",
+      "PAUSE": "\\u{E00B}",
+      "ESCAPE": "\\u{E00C}",
+      "SPACE": "\\u{E00D}",
+      "PAGE_UP": "\\u{E00E}",
+      "PAGE_DOWN": "\\u{E00F}",
+      "END": "\\u{E010}",
+      "HOME": "\\u{E011}",
+      "LEFT": "\\u{E012}",
+      "ARROW_LEFT": "\\u{E012}",
+      "UP": "\\u{E013}",
+      "ARROW_UP": "\\u{E013}",
+      "RIGHT": "\\u{E014}",
+      "ARROW_RIGHT": "\\u{E014}",
+      "DOWN": "\\u{E015}",
+      "ARROW_DOWN": "\\u{E015}",
+      "INSERT": "\\u{E016}",
+      "DELETE": "\\u{E017}",
+      "SEMICOLON": "\\u{E018}",
+      "EQUALS": "\\u{E019}",
+      "NUMPAD0": "\\u{E01A}",
+      "NUMPAD1": "\\u{E01B}",
+      "NUMPAD2": "\\u{E01C}",
+      "NUMPAD3": "\\u{E01D}",
+      "NUMPAD4": "\\u{E01E}",
+      "NUMPAD5": "\\u{E01F}",
+      "NUMPAD6": "\\u{E020}",
+      "NUMPAD7": "\\u{E021}",
+      "NUMPAD8": "\\u{E022}",
+      "NUMPAD9": "\\u{E023}",
+      "MULTIPLY": "\\u{E024}",
+      "ADD": "\\u{E025}",
+      "SEPARATOR": "\\u{E026}",
+      "SUBTRACT": "\\u{E027}",
+      "DECIMAL": "\\u{E028}",
+      "DIVIDE": "\\u{E029}",
+      "F1": "\\u{E031}",
+      "F2": "\\u{E032}",
+      "F3": "\\u{E033}",
+      "F4": "\\u{E034}",
+      "F5": "\\u{E035}",
+      "F6": "\\u{E036}",
+      "F7": "\\u{E037}",
+      "F8": "\\u{E038}",
+      "F9": "\\u{E039}",
+      "F10": "\\u{E03A}",
+      "F11": "\\u{E03B}",
+      "F12": "\\u{E03C}",
+      "META": "\\u{E03D}",
+      "COMMAND": "\\u{E03D}",
+      "ZENKAKU_HANKAKU": "\\u{E040}"
+    };
+  
     if (stepType.name.startsWith("store") && pName == "variable") { return value; }
     if (stepType.name == "switchToFrameByIndex" && pName == "index") { return value; }
     // This function takes a string literal and escapes it and wraps it in quotes.
@@ -279,6 +353,10 @@ builder.selenium2.io.addLangFormatter({
     var hasDollar = false; // Whether we've just encountered a $ character.
     var insideVar = false; // Whether we are reading in the name of a variable.
     var varName = "";      // Accumulates letters of the current variable.
+    var hasBang = false;   // Whether we've just encountered a ! character.
+	  var insideKey = false; // Whether we're reading a key escape sequence.
+    var keyName = "";      // Accumulates letters of the current key.
+    
     for (var i = 0; i < value.length; i++) {
       var ch = value.substring(i, i + 1);
       if (insideVar) {
@@ -286,7 +364,7 @@ builder.selenium2.io.addLangFormatter({
           // We've finished reading in the name of a variable.
           // If this isn't the start of the expression, use + to concatenate it.
           if (output.length > 0) { output += " . "; }
-          output += "$test->" + varName;
+          output += "$" + varName;
           insideVar = false;
           hasDollar = false;
           varName = "";
@@ -294,6 +372,19 @@ builder.selenium2.io.addLangFormatter({
           // This letter is part of the name of the variable we're reading in.
           varName += ch;
         }
+	    } else if (insideKey) {
+	      if (ch == "}") {
+	        // We've finished reading in the name of a key.
+	        // If this isn't the start of the expression, use . to concatenate it.
+	        if (output.length > 0) { output += " . "; }
+	        output += "\"" + keysMap[keyName] + "\"";
+	        insideKey = false;
+	        hasBang = false;
+	        keyName = "";
+	      } else {
+	        // This letter is part of the name of the key we're reading in.
+	        keyName += ch;
+	      }
       } else {
         // We're not currently reading in the name of a variable.
         if (hasDollar) {
@@ -312,10 +403,33 @@ builder.selenium2.io.addLangFormatter({
             hasDollar = false;
             lastChunk += "$" + ch;
           }
+        } else if (hasBang) {
+	        // But we *have* just encountered a !, so if this character is a {, we are about to
+	        // do a key.
+	        if (ch == "{") {
+	          insideKey = true;
+	          if (lastChunk.length > 0) {
+	            // Add the literal we've read in to the text.
+	            if (output.length > 0) { output += " . "; }
+	            output += esc(lastChunk);
+	          }
+	          lastChunk = "";
+	        } else {
+	          // No, it was just a lone !.
+	          hasBang = false;
+	          lastChunk += "!" + ch;
+	        }
         } else {
           // This is the "normal case" - accumulating the letters of a literal. Unless the letter
-          // is a $, in which case this may be the start of a...
-          if (ch == "$") { hasDollar = true; } else { lastChunk += ch; }
+	        // is a $, in which case this may be the start of a variable. Or a !, in which case it
+	        // may be part of a key.
+	        if (ch == "$") {
+	          hasDollar = true;
+	        } else if (ch == "!") {
+	          hasBang = true;
+	        } else {
+	          lastChunk += ch;
+	        }
         }
       }
     }
